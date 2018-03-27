de0bb6c940525ee8c8e129c6d4a8cf06371182c5
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-27 19:59:15 -0400

    Add internal state to Login component, wire up submissions to update state

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,20 +5,40 @@ import { login } from '../store';
 
 
 class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e){
+        const {username, password} = this.state;
+        e.preventDefault();
+        this.props.login(username, password);
+        this.props.history.push('/'); //home page
+    }
     render() {
         return (
             <div>
-                <form onSubmit={e => {
-                    e.preventDefault();
-                    this.props.login('admin','password');
-                    this.props.history.push('/'); //home page
-                }}>
+                <form onSubmit={this.handleSubmit}>
                     <h2>Login:</h2>
                     <div>
-                        <input type="email" placeholder="Email..." required />
+                        <input 
+                            type="email" 
+                            value={this.state.username}
+                            onChange={(e) => this.setState({username: e.target.value})}
+                            placeholder="Email..." 
+                            required />
                     </div>
                     <div>
-                        <input type="password" placeholder="Password..." required />
+                        <input 
+                            type="password" 
+                            value={this.state.password}
+                            onChange={(e) => this.setState({password: e.target.value})}
+                            placeholder="Password..." 
+                            required />
                     </div>
                     <button onClick={''}>Login</button>
 
